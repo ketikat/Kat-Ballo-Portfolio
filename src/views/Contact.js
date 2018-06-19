5024833b527307c31a1e0bba2f6aa1cdbd464aa6
kat ballo <katballo@icloud.com>
2018-06-19 18:00:36 -0400

    cv link fix

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -36,7 +36,7 @@ export default function Contact (props) {
           </div>
 
           <div id='cvLink'>
-            <a href='../../katballo-CV.pdf' download='Kat_Ballo-CV.pdf'>
+            <a href='../../Kat_Ballo-CV.pdf' download='Kat_Ballo-CV.pdf'>
               <img src='../cv.svg' className='fa' />
               <p> download</p>
             </a>
